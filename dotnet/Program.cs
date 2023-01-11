4ea7cfa1a283283e39cdf4af9f55590204c10dd0
Iurikman <y.m.kutyrev@gmail.com>
2023-01-11 23:35:34 +0300

    Добавлены 2 вспомогательных модуля

--- dotnet/Program.cs
+++ dotnet/Program.cs
@@ -1,30 +1,8 @@
-﻿
-// Сортировка выбором
-
-void SortArray(int[] array)
-{
-    for (int i = 0; i < array.Length; i++)
-    {
-        for (int j = i + 1; j < array.Length; j++)
-        {
-            if (array[i] > array[j])
-            {
-                int temp = array[i];
-                array[i] = array[j];
-                array [j] = temp;
-            }
-        }
-    }
-}
-
-int[] array = {0, 7, 5, 4, 4, 2, 9, 0 ,9};
-
-void ShowArray(int[] array)
-{
-    Console.WriteLine($"[{String.Join(',', array)}]");
-}
+﻿using static Sorting;
+using static Infrastructure;
 
 
+int[] array = CreateArray(size: 10, min: 1, max:10);
 ShowArray(array);
 SortArray(array);
 ShowArray(array);
